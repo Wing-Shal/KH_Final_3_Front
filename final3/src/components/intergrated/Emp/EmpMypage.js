20f90a7d58c3058ac356b741e4bdc20c050648d4
shw2336 <shw2336@naver.com>
2024-05-09 18:00:36 +0900

    EmpMypage수정, Calender사진

--- final3/src/components/intergrated/Emp/EmpMypage.js
+++ final3/src/components/intergrated/Emp/EmpMypage.js
@@ -1,32 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "../../utils/CustomAxios";
 import { loginIdState } from '../../utils/RecoilData';
 import { useRecoilState } from 'recoil';
-// import axios from 'axios';
+import calender from "../../../assets/calender.png";
+import { Link } from 'react-router-dom';
 
 function EmpMypage() {
   const [imagePreview, setImagePreview] = useState(null);
   const [file, setFile] = useState(null);
   const [loginId, setLoginId] = useRecoilState(loginIdState);
+  const [projects, setProjects] = useState([]); // 프로젝트 목록 상태 추가
+  const [empInfo, setEmpInfo] = useState(null); // 사원 정보 추가
+  const [showModal, setShowModal] = useState(false);
 
-  // empNo 값 변경
-  // const handleChangeEmpNo = () => {
-  //   setEmpNo(123) // 새로운 empNo 값으로 업데이트
-  // };
+  // 모달 열기
+  const openModal = () => {
+    setShowModal(true);
+  };
 
-  const clearImagePreview = () => {
-    setImagePreview(null);
-    setFile(null); // 파일 초기화
+  // 모달 닫기
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+
+  useEffect(() => {
+    loadProjects(); // 프로젝트 목록 불러오기
+    loadEmpData(); // 사원 정보 불러오기
+
+    // 페이지가 렌더링될 때 로컬 스토리지에서 이미지를 가져와서 설정
+    const savedImage = localStorage.getItem('savedImage');
+    if (savedImage) {
+      setImagePreview(savedImage);
+    }
+  }, []);
+
+  const loadEmpData = async () => {
+    try {
+      const token = axios.defaults.headers.common['Authorization'];
+      const resp = await axios.get(`emp/${token}`);
+      setEmpInfo(resp.data);
+    } catch (error) {
+      console.error('사원 정보 불러오기 오류:', error);
+    }
+  };
+
+  const loadProjects = async () => {
+    try {
+      const empNo = loginId;
+      const resp = await axios.get("/project/" + empNo);
+      setProjects(resp.data);
+      console.log(resp.data);
+    } catch (error) {
+      console.error('프로젝트 목록 불러오기 오류:', error);
+    }
   };
 
   const handleImageChange = e => {
     const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setImagePreview(reader.result);
-    };
     if (file) {
       setFile(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+        // 이미지를 선택한 후 로컬 스토리지에 저장
+        localStorage.setItem('savedImage', reader.result);
+      };
       reader.readAsDataURL(file);
     }
   };
@@ -36,22 +75,15 @@ function EmpMypage() {
       if (file) {
         const formData = new FormData();
         formData.append('attach', file);
-    
-        const response = await axios.post("/emp/upload/"+loginId, formData, {
+
+        const response = await axios.post("/emp/upload/" + loginId, formData, {
           headers: {
             'Content-Type': 'multipart/form-data'
           }
         }, [loginIdState]);
         console.log('파일 업로드 결과:', response.data);
-    
-        // 여기서 response.data에는 DB에 저장된 정보가 있을 수 있으므로, 필요에 따라 처리할 수 있습니다.
-        // 예를 들어, DB에 저장된 파일의 ID나 URL 등을 가져와서 표시하거나 다른 작업을 할 수 있습니다.
-        
-        // DB에 저장되었다는 메시지를 출력하거나 다른 작업을 할 수 있습니다.
+
         console.log('DB에 저장되었습니다.');
-    
-        // 저장 후에는 이미지 미리보기를 초기화할 수 있습니다.
-        clearImagePreview();
       } else {
         console.log('파일이 선택되지 않았습니다.');
       }
@@ -59,26 +91,90 @@ function EmpMypage() {
       console.error('파일 업로드 오류:', error);
     }
   };
-  
+
   return (
     <>
       <h1>마이페이지</h1>
-      <div className="container-lg border border-5 rounded p-3 mb-3">
-
-        <div className="row align-items-center ">
-          <div>
-            <span className="font-style">사진</span>
-            <input type="file" onChange={handleImageChange} className="form-control"
-              id="upload" aria-label="upload" />
-            {imagePreview && (
-              <img src={imagePreview} alt="사진 미리보기" style={{ maxWidth: '200px', maxHeight: '200px' }} />
+      <div className="container-sm border border-5 rounded p-3 mb-3">
+        <div className="row align-items-center">
+          {/* 사진 첨부 파일 */}
+          <div className="col-md-3">
+            <div>
+              <input type="file" onChange={handleImageChange} className="form-control form-control-sm"
+                id="upload" aria-label="upload" style={{ maxWidth: '200px', maxHeight: '200px' }} />
+              <br />
+              {imagePreview && (
+                <img src={imagePreview} alt="사진 미리보기" style={{ maxWidth: '200px', maxHeight: '200px', marginBottom: '10px' }} />
+              )}
+            </div>
+            <br />
+            <button onClick={handleSave} className="btn btn-sm btn-primary" style={{ maxWidth: '200px', maxHeight: '200px', marginLeft: '10px' }}>저장</button>
+          </div>
+  
+          {/* 개인 정보 */}
+          <div className="col-md-5">
+            {empInfo && (
+              <table className="table">
+                <tbody>
+                  <React.Fragment key={empInfo.empNo}>
+                    <tr>
+                      <td>사원명</td>
+                      <td>{empInfo.empName}</td>
+                    </tr>
+                    <tr>
+                      <td>사원번호</td>
+                      <td>{empInfo.empNo}</td>
+                    </tr>
+                    <tr>
+                      <td>소속부서</td>
+                      <td>{empInfo.empDept}</td>
+                    </tr>
+                    <tr>
+                      <td>연락처</td>
+                      <td>{empInfo.empContact}</td>
+                    </tr>
+                    <tr>
+                      <td>이메일</td>
+                      <td>{empInfo.empEmail}</td>
+                    </tr>
+                    <tr>
+                      <td>자기소개</td>
+                      <td>{empInfo.empPr}</td>
+                    </tr>
+                  </React.Fragment>
+                </tbody>
+              </table>
             )}
           </div>
-          <button onClick={handleSave}>저장</button> {/* 저장 버튼 */}
-          {/* 나머지 부분 생략 */}
+  
+          {/* 달력 이미지 */}
+          <div className="col-md-4">
+            <div className="container-sm border border-5 rounded p-3 mb-3">
+            <img className='calender' src={ calender } alt="달력" style={{ maxWidth: '100%', height: 'auto' }} />
+            </div>
+          </div>
         </div>
+      </div>
+  
+      {/* 프로젝트 목록 */}
+      <div className="container-sm border border-5 rounded p-3 mb-3">
+        {/* 프로젝트 목록 렌더링 */}
+        <div><h2>내 프로젝트 목록</h2></div>
+        <br></br>
+        {projects.map(project => (
+          <div>
+            <Link to={`/document/project/:${project.projectNo}`}>
+            <p> - {project.projectName}</p>
+              </Link>
+          </div>
+        ))}
       </div>
     </>
   );
+
+  
+  
+  
 }
+
 export default EmpMypage;
